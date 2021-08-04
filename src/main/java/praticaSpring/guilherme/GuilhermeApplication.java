e66dad211a910c81a5a199c0d4e9b5d82be1a4b4
guilherme de brida de bona <guilherme.bona@betha.com.br>
2021-08-04 08:24:55 -0300

    First commit

---
+++ src/main/java/praticaSpring/guilherme/GuilhermeApplication.java
@@ -0,0 +1,13 @@
+package praticaSpring.guilherme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/praticaSpring/guilherme/GuilhermeApplicationTests.java
@@ -0,0 +1,13 @@
+package praticaSpring.guilherme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilhermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
