0b592dba569ba8813d65b3cb31853f843684a2df
guilherme de brida de bona <guilherme.bona@betha.com.br>
2021-08-09 08:41:36 -0300

    Postagens

--- frontend/js/controlers/postagensCtrl.js
+++ frontend/js/controlers/postagensCtrl.js
@@ -1,5 +1,25 @@
 angular.module("appPostagens").controller("postagensCtrl", function ($scope, postagensAPI) {
+    $scope.app = "appPostagens";
+    $scope.post = {};
 
 
 
+    var _formatarcomentarios = function (post) {
+        return post.comentario
+            .map(function (comentario) {
+                return "id: " + comentario.id  + " Conteudo: " + comentario.text;
+            })
+            .join(", ");
+    };
+    
+    var getPostagens = function () {
+        postagensAPI.getPostagens().then(function (retorno) {
+            $scope.postagens = retorno.data;
+            console.log( $scope.postagens)
+        });
+    };
+
+
+    $scope.formatarComentarios = _formatarcomentarios;
+    getPostagens()
 });
--- frontend/js/services/postagensService.js
+++ frontend/js/services/postagensService.js
@@ -1,5 +1,13 @@
 angular.module("appPostagens").factory("postagensAPI", function($http, config){
 
 
+    var _getPostagens = function(){
+        return $http.get(config.baseUrl + "/postagens")
+    }
+
   
+    return{
+        getPostagens:_getPostagens,
+    }
+
 })
